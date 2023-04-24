dfc0d37cd0765d0431d94b8183cb4096c6219bb6
adadhore <107457695+adadhore@users.noreply.github.com>
2023-04-24 14:47:26 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket_acl" "data" {
+  bucket = "${local.resource_prefix.value}-data"
+  acl    = "private"
+}
+resource "aws_s3_bucket" "data" {
+  # bucket is public
+  # bucket is not encrypted
+  # bucket does not have access logs
+  # bucket does not have versioning
+  bucket = "${local.resource_prefix.value}-data"
+  tags = {
+    yor_trace = "ccf11b7f-e248-4980-bdb7-84ab30a83244"
+    owner     = "cmartinsjr_paloaltonetworks_com"
+  }
+}
+resource "aws_s3_bucket_public_access_block" "data" {
+  bucket = "${local.resource_prefix.value}-data"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
